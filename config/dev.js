7c87e33e8db85d5102ed0e0b02b9e91cacf1b6c8
kamalpandey484 <kamal.pandey@tothenew.com>
2021-04-28 17:30:50 +0530

    basic setup completed.

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,12 @@
+console.log(
+  {"1": process.env.googleClientID,
+  "2": process.env.googleClientSecret,
+  "3": process.env.mongoURI,
+  "4": process.env.cookieKey}
+);
 module.exports = {
-  googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123',
+  googleClientID: process.env.googleClientID,
+  googleClientSecret: process.env.googleClientSecret,
+  mongoURI: process.env.mongoURI,
+  cookieKey: process.env.cookieKey,
 };
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
+
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const bodyParser = require('body-parser');
